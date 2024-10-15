fafec8f5e8c8795a80beb7c1b20b7f9758d216e7
agriapriliando <agripriliando@gmail.com>
2024-10-15 22:48:14 +0700

    route pemohon

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -24,7 +24,7 @@ class Login extends Component
             if (Auth::user()->role == 'admin') {
                 return redirect()->route('admin.appreq', 'diajukan');
             } else {
-                return redirect()->route('appreq.list');
+                return redirect()->route('appreq.create');
             }
         } else {
             session()->flash('error', 'Username atau Password salah');
